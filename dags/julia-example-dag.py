3ac1951b65cc99b5343cca9c50c439bb8fc0aab8
Julia Eskew <jeskew@edx.org>
2019-11-25 13:55:20 -0500

    Fix timedelta call

--- dags/julia-example-dag.py
+++ dags/julia-example-dag.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 default_args = {
 	'owner': 'airflow',
 	'depends_on_past': False,
-	'start_date': datetime.utcnow() - datetime.timedelta(days=1),
+	'start_date': datetime.utcnow() - timedelta(days=1),
 	'email_on_failure': False,
 	'email_on_retry': False,
 	'retries': 1,
